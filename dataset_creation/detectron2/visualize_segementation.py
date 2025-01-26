2959e072327f5c77886fa2c2012065f5f58b3351
kameel <kameel.amareen@tum.de>
2025-01-26 20:04:44 +0100

    Added Class based visualization

--- dataset_creation/detectron2/visualize_segementation.py
+++ dataset_creation/detectron2/visualize_segementation.py
@@ -1,10 +1,13 @@
 import cv2
 import numpy as np
 import os
+import json
 
-DATASET_PATH = "/workspace/Datasets/docking_3_synced"
+# DATASET_PATH = "/workspace/Datasets/docking_3_synced"
+DATASET_PATH = "/workspace/Datasets/flat_dataset"
 RUN_NAME = "run1"
 MAX_LABELS = 134
+VISUALIZE_CLASS = True  # Else Coloured by Instance
 np.random.seed(40)
 
 
@@ -47,6 +50,18 @@ def visualize_segmentation(image, prediction, id_color_map):
     return blended
 
 
+def create_instance_to_class_map(labels):
+    """
+    Create a mapping from instance IDs to class IDs.
+    """
+    instance_to_class_map = {}
+    for label in labels:
+        instance_to_class_map[label["id"]] = label["category_id"]
+    # Adding the Unknown class
+    instance_to_class_map[0] = 0
+    return instance_to_class_map
+
+
 def main():
     # Check if directory exists
     if not os.path.exists(os.path.join(DATASET_PATH, RUN_NAME)):
@@ -69,17 +84,34 @@ def main():
         if f.endswith("_predicted.png")
     ]
     predictions.sort()
+    labels = [
+        f
+        for f in os.listdir(os.path.join(DATASET_PATH, RUN_NAME))
+        if f.endswith("_labels.json")
+    ]
+    labels.sort()
 
     # Generate color map
     id_color_map = generate_colour_code(MAX_LABELS)
 
     # Process each image-prediction pair
-    for img_name, pred_name in zip(images, predictions):
+    for img_name, pred_name, label_name in zip(images, predictions, labels):
         # Load images
         image = cv2.imread(os.path.join(DATASET_PATH, RUN_NAME, img_name))
         prediction = cv2.imread(
             os.path.join(DATASET_PATH, RUN_NAME, pred_name), cv2.IMREAD_GRAYSCALE
         )
+
+        # Load JSON labels
+        with open(os.path.join(DATASET_PATH, RUN_NAME, label_name), "r") as file:
+            label = json.load(file)
+            instance_to_class = create_instance_to_class_map(label)
+            print(instance_to_class)
+
+        # Convert instance IDs to class IDs
+        if VISUALIZE_CLASS:
+            # Convert instance IDs to class IDs
+            prediction = np.vectorize(instance_to_class.get)(prediction)
 
         # Visualize segmentation
         result = visualize_segmentation(image, prediction, id_color_map)
